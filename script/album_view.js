066e80232f0d54574ce856c73c5a5fd70c5e124f
MariaKilsved <40439902+MariaKilsved@users.noreply.github.com>
2022-09-02 10:51:25 +0200

    album_view design

--- script/album_view.js
+++ script/album_view.js
@@ -14,15 +14,27 @@ window.addEventListener('DOMContentLoaded', async () => {
 library = await lib.pictureLibraryBrowser.fetchJSON(libraryJSON);  //reading library from JSON on local server 
 //library = lib.pictureLibraryBrowser.createFromTemplate();  //generating a library template instead of reading JSON
 
-for (const album of library.albums) {
+//Obtain album from URL:
+//let albumQuery = window.location.search.substring(1);
 
-    renderImage(album.headerImage, album.id);
-    for (const picture of album.pictures) {
-      renderImage(`${album.path}/${picture.imgLoRes}`, picture.id);
-      renderImage(`${album.path}/${picture.imgHiRes}`, picture.id);
-    }
+let iterator = 0;
+
+for (const album of library.albums) {
+    /*if(album.id == albumQuery) {*/
+      for (const picture of album.pictures) {
+        iterator++;
+        renderImage(
+          `${album.path}/${picture.imgLoRes}`, 
+          `${album.path}/${picture.imgHiRes}`, 
+          picture.id, 
+          picture.title, 
+          picture.comment,
+          iterator
+        );
+      }
+    /*}*/
   }
-})
+});
 
 window.addEventListener('click',  () => {
 
@@ -31,16 +43,214 @@ window.addEventListener('click',  () => {
 });
 
 //Render the images
-function renderImage(src, tag) {
+function renderImage(loResSrc, hiResSrc, tag, title, comment, iterator) {
+/*<div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">....</li>
+    <li class="list-group-item">....</li>
+  </ul>
+</div>*/
 
-  const div = document.createElement('div');
-  div.className = `FlexItem`;
-  div.dataset.albumId = tag;
+/*---Small screens - 1 column---*/
 
+  //<div class="card">
+  var card = document.createElement('div');
+  card.className = `card`;
+  card.dataset.albumId = tag;
+
+  //<img src="..." class="card-img-top" alt="...">
   const img = document.createElement('img');
-  img.src = src;
-  div.appendChild(img);
+  img.src = hiResSrc;
+  img.srcset = `${loResSrc}, ${hiResSrc} 2x`;    //Actual width is unknown
+  img.loading = `lazy`;
+  img.className = `card-img-top`;
+  img.alt = title;
+  card.appendChild(img);
 
-  const imgFlex = document.querySelector('.FlexWrap');
-  imgFlex.appendChild(div);
+  //<ul class="list-group list-group-flush">
+  const ul = document.createElement('ul');
+  ul.className = `list-group list-group-flush`;
+
+  //<li class="list-group-item">....</li>
+  const li1 = document.createElement('li');
+  li1.className = `list-group-item`;
+  const text1 = document.createTextNode(title);
+  li1.appendChild(text1);
+
+  //<li class="list-group-item">....</li>
+  const li2 = document.createElement('li');
+  li2.className = `list-group-item`;
+  const text2 = document.createTextNode(comment);
+  li2.appendChild(text2);
+
+  //</ul>
+  ul.appendChild(li1);
+  ul.appendChild(li2);
+  card.appendChild(ul);
+  //</div>
+
+  const col1 = document.getElementById('c1');
+  col1.appendChild(card);
+
+/*----------------------------------------------------------------------*/
+/*---Semi-small screens - 2 columns---*/
+
+  //<div class="card">
+  var card2 = document.createElement('div');
+  card2.className = `card`;
+  card2.dataset.albumId = tag;
+
+  //<img src="..." class="card-img-top" alt="...">
+  const img2 = document.createElement('img');
+  img2.src = hiResSrc;
+  img2.srcset = `${loResSrc}, ${hiResSrc} 2x`;    //Actual width is unknown
+  img2.loading = `lazy`;
+  img2.className = `card-img-top`;
+  img2.alt = title;
+  card2.appendChild(img2);
+
+  //<ul class="list-group list-group-flush">
+  const ul2 = document.createElement('ul');
+  ul2.className = `list-group list-group-flush`;
+
+  //<li class="list-group-item">....</li>
+  const li1_2 = document.createElement('li');
+  li1_2.className = `list-group-item`;
+  const text1_2 = document.createTextNode(title);
+  li1_2.appendChild(text1_2);
+
+  //<li class="list-group-item">....</li>
+  const li2_2 = document.createElement('li');
+  li2_2.className = `list-group-item`;
+  const text2_2 = document.createTextNode(comment);
+  li2_2.appendChild(text2_2);
+
+  //</ul>
+  ul2.appendChild(li1_2);
+  ul2.appendChild(li2_2);
+  card2.appendChild(ul2);
+  //</div>
+
+  if(iterator % 2 === 1) {
+    const col2 = document.getElementById('c2');
+    col2.appendChild(card2);
+  }
+  else {
+    const col3 = document.getElementById('c3');
+    col3.appendChild(card2);
+  }
+
+/*----------------------------------------------------------------------*/
+/*---Semi-large screens - 3 columns---*/
+
+  //<div class="card">
+  var card3 = document.createElement('div');
+  card3.className = `card`;
+  card3.dataset.albumId = tag;
+
+  //<img src="..." class="card-img-top" alt="...">
+  const img3 = document.createElement('img');
+  img3.src = hiResSrc;
+  img3.srcset = `${loResSrc}, ${hiResSrc} 2x`;    //Actual width is unknown
+  img3.loading = `lazy`;
+  img3.className = `card-img-top`;
+  img3.alt = title;
+  card3.appendChild(img3);
+
+  //<ul class="list-group list-group-flush">
+  const ul3 = document.createElement('ul');
+  ul3.className = `list-group list-group-flush`;
+
+  //<li class="list-group-item">....</li>
+  const li1_3 = document.createElement('li');
+  li1_3.className = `list-group-item`;
+  const text1_3 = document.createTextNode(title);
+  li1_3.appendChild(text1_3);
+
+  //<li class="list-group-item">....</li>
+  const li2_3 = document.createElement('li');
+  li2_3.className = `list-group-item`;
+  const text2_3 = document.createTextNode(comment);
+  li2_3.appendChild(text2_3);
+
+  //</ul>
+  ul3.appendChild(li1_3);
+  ul3.appendChild(li2_3);
+  card3.appendChild(ul3);
+  //</div>
+
+  if(iterator % 3 === 1) {
+    const col4 = document.getElementById('c4');
+    col4.appendChild(card3);
+  }
+  else if (iterator % 3 === 2){
+    const col5 = document.getElementById('c5');
+    col5.appendChild(card3);
+  }
+  else {
+    const col6 = document.getElementById('c6');
+    col6.appendChild(card3);
+  }
+
+/*----------------------------------------------------------------------*/
+/*---Large screens - 4 columns---*/
+
+
+  //<div class="card">
+  var card4 = document.createElement('div');
+  card4.className = `card`;
+  card4.dataset.albumId = tag;
+
+  //<img src="..." class="card-img-top" alt="...">
+  const img4 = document.createElement('img');
+  img4.src = hiResSrc;
+  img4.srcset = `${loResSrc}, ${hiResSrc} 2x`;    //Actual width is unknown
+  img4.loading = `lazy`;
+  img4.className = `card-img-top`;
+  img4.alt = title;
+  card4.appendChild(img4);
+
+  //<ul class="list-group list-group-flush">
+  const ul4 = document.createElement('ul');
+  ul4.className = `list-group list-group-flush`;
+
+  //<li class="list-group-item">....</li>
+  const li1_4 = document.createElement('li');
+  li1_4.className = `list-group-item`;
+  const text1_4 = document.createTextNode(title);
+  li1_4.appendChild(text1_4);
+
+  //<li class="list-group-item">....</li>
+  const li2_4 = document.createElement('li');
+  li2_4.className = `list-group-item`;
+  const text2_4 = document.createTextNode(comment);
+  li2_4.appendChild(text2_4);
+
+  //</ul>
+  ul4.appendChild(li1_4);
+  ul4.appendChild(li2_4);
+  card4.appendChild(ul4);
+  //</div>
+
+  if(iterator % 4 === 1) {
+    const col7 = document.getElementById('c7');
+    col7.appendChild(card4);
+  }
+  else if (iterator % 4 === 2){
+    const col8 = document.getElementById('c8');
+    col8.appendChild(card4);
+  }
+  else if (iterator % 4 === 3) {
+    const col9 = document.getElementById('c9');
+    col9.appendChild(card4);
+  }
+  else {
+    const col10 = document.getElementById('c10');
+    col10.appendChild(card4);
+  }
 };
+
+
+
+
