8b07b0f2cec9854c90e946fa932a9b18874448bb
AlexandraSammueang <alexandra_salen@hotmail.com>
2022-09-08 09:45:46 +0200

    .

--- script/upload_image.js
+++ script/upload_image.js
@@ -27,19 +27,93 @@ document.addEventListener('DOMContentLoaded', ()=>{
    
 });
 
-const http = require("http");
-const server = http.createServer(function(req,res) {
-res.setHeader("Content-type", "application/json");
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.writeHead(200); //status code HTTP 200 / OK
+// const http = require("http");
+// const server = http.createServer(function(req,res) {
+// res.setHeader("Content-type", "application/json");
+// res.setHeader("Access-Control-Allow-Origin", "*");
+// res.writeHead(200); //status code HTTP 200 / OK
 
-res.end(images1);
-});
-server.listen(3000, function(){
-  console.log("Listenig on port 3000");
-});
+// res.end(images1);
+// });
+// server.listen(3000, function(){
+//   console.log("Listenig on port 3000");
+// });
 
 /*addImage.innerHTML = "Image" + title.value + myFile.value;*/
 
+localStorage.setItem('star', 'theStarClickedID');
+var star = localStorage.getItem('star');
+
+// const path = require('path');
+// const fs = require('fs');
+
+// //from the downloaded npm
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const port = 3000;
+
+// //initialize cors
+// app.use(cors({
+//   origin: '*'
+// }));
+
+// //send a simple Hello World in response to 
+// //http://localhost:3000
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// //sending a simple json string in response to 
+// //http://localhost:3000/ingredients
+// app.get('/ingredients', (req, res) => {
+
+//   response = readJSON(`ingedients.json`);
+//   res.send(response);
+// });
 
 
+// //Start listening
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// })
+
+
+
+// //Initialize application data
+// function initAppData() {
+
+//   //Here we can create some initial application data
+//    const ingredients = [
+//     {
+//       "id": "1",
+//       "item": "Bacon"
+//     },
+//     {
+//       "id": "2",
+//       "item": "Eggs"
+//     },
+//     {
+//       "id": "3",
+//       "item": "Milk"
+//     },
+//     {
+//       "id": "4",
+//       "item": "Butter"
+//     }
+//   ];
+ 
+//   writeJSON(`ingedients.json`, ingredients);
+// }
+
+// //helper functions to read and write JSON
+// function writeJSON(fname, obj) {
+//   const appDataDir = path.join(__dirname, `app_data`);
+//   fs.writeFileSync(path.resolve(appDataDir, fname), JSON.stringify(obj));
+// }
+
+// function readJSON(fname) {
+//   const appDataDir = path.join(__dirname, `app_data`);
+//   obj = JSON.parse(fs.readFileSync(path.resolve(appDataDir, fname), 'utf8'));
+//   return obj;
+// }
