9e4a21a688cf223dac748ca0d7425f92e2601024
AlexandraSammueang <alexandra_salen@hotmail.com>
2022-09-15 22:49:46 +0200

    create album NOT Done

---
+++ script/new_album.js
@@ -0,0 +1,38 @@
+'use strict';  // Try without strict mode
+
+const addDictoryServer = document.getElementById('addDictoryServer');
+// const urlPost = 'http://localhost:3000/api/upload/album';
+// const urlJson = './app-data/library/picture-library.json';
+// ./ leta utanför map
+
+
+addDictoryServer.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    //Create the key/value pairs used in the form
+    const formData = new FormData(addDictoryServer);
+    try {
+        //send the data using post and await the reply
+        const response = await fetch(urlPost, {
+            method: 'post',
+            body: formData
+        });
+        const result = await response.text();
+
+        if (response.ok) {
+
+            const response = await fetch(urlJson);
+            const data = await response.text();
+
+            alert(`Submitting the information. It has been recieved:\n`+
+                `${data}`);
+        }
+        else {
+            alert("Transmission error");
+        }
+        console.log(result);
+    }
+    catch {
+        alert("Transmission error");
+    }
+})
--- server/server-main.js
+++ server/server-main.js
@@ -21,11 +21,7 @@ app.get('/api/upload', (req, res) => {
   response = readJSON(appJson);
   res.send(response);
 });
-/* 
-app.get('/', (req, res) => {
-  res.send(`Some kind of string in here`);
-});
-*/
+
 
 app.post('/api/upload', (req, res) => {
   const b = req.body;
@@ -35,13 +31,6 @@ app.post('/api/upload', (req, res) => {
   res.json(b);
 });
 
-/*
-app.put('/api/update', (req, res) => {
-  res.send("PUT Request Called");
-
-  writeJSON(appJson, (req.body));
-});
-*/
 
 app.listen(port, () =>
   console.log(`Node server listening at http://localhost:${port}`)
@@ -120,5 +109,90 @@ function writeJSON(fname, obj) {
 function readJSON(fname) {
   const appDataDir = path.normalize(path.join(__dirname, appDir));
   obj = JSON.parse(fs.readFileSync(path.join(appDataDir, fname), 'utf8'));
+  return obj;
+}
+
+
+//DETTA ÄR TILLLAGT NUUUUU
+//Post request
+app.post('/api/upload/album', (req, res) => {
+
+  //Creates a formidable object of the incoming data
+  let form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+
+      // fs.mkdirSync(path.resolve(`app-data/library/pictures/${fields.title}`));
+
+      let newName = files.myImage.originalFilename.trim().replace(' ', '-').replace(/(\s|-|_|~)+/g, '-').toLowerCase();
+      let oldPath = files.myImage.filepath;
+      let title = fields.title.trim().replace(' ', '-').replace(/(\s|-|_|~)+/g, '-').toLowerCase();
+      const dir = `./app-data/library/pictures/${title}`;
+
+      let newPath = './app-data/library/pictures/album-header/' +  newName;
+
+      console.log(newPath)
+
+      fs.mkdirSync(dir, { recursive: true }, (err) => {
+          if (err) {
+              throw err;
+          }
+      });
+
+      const data = fs.readFileSync(oldPath)
+
+      fs.writeFileSync(newPath, data, function(err){
+          res.status(501).send('Couldnt create album');
+          return;
+      })
+
+      let libraryJson = JSON.parse(fs.readFileSync(path.resolve('./app-data', 'library/' + 'picture-library.json'), 'utf8'));
+
+      // libraryJson = JSON.parse(path.resolve(albumHeaderDir, libraryJsonPath));
+      // console.log(libraryJson.albums);
+
+      let albumObj = {
+          id:  uniqueId(),
+          title: title,
+          comment: fields.albumComment.trim().replace(' ', '-').replace(/(\s|-|_|~)+/g, '-').toLowerCase(),
+          path: dir,
+          headerImage: newPath,
+          pictures: [],
+      };
+
+      libraryJson.albums.push(albumObj);
+
+      //fs skriv fil syncront, skriver över jasonfil och lägger på ny jason fil som skapas 
+      fs.writeFileSync(libraryJsonPath, JSON.stringify(libraryJson), function(err) {
+          // Todo: remove album header picture and directory in case of an error
+          res.sendStatus(501); //501 = fel meddelande 
+          return;
+      });
+      res.sendStatus(200); // 200 = OK
+  });
+});
+
+
+
+//Writes image info to albumCache.json to later unpack and use
+// writeJSON('albumCache.json', {err, fields, files});
+// res.json({ fields, files });
+
+//Converts file info to JSON
+
+
+function uniqueId() {
+  const dateString = Date.now().toString(36);
+  const randomness = Math.random().toString(36).substring(2);
+  return dateString + randomness;
+};
+function writeJSON(fname, obj) {
+  const dir = path.join(applicationDir, `/${libraryDir}`);
+  fs.writeFileSync(path.resolve(dir, fname), JSON.stringify(obj));
+}
+
+function readJSON(fname) {
+  const dir2 = path.join(applicationDir, `/${libraryDir}`);
+
+  const obj = JSON.parse(fs.readFileSync(path.resolve(dir2, fname), 'utf8'));
   return obj;
 }
