5a9758abf8fd86755f9350099daca13ceb3e623b
Kavehlito <kajaliliyan@gmail.com>
2022-09-16 18:44:09 +0200

    frontend for uploadimage done, need to fix backend

--- script/upload_image.js
+++ script/upload_image.js
@@ -1,5 +1,97 @@
 'use strict';  // Try without strict mode
 
+import * as lib from '../model/picture-library-browser.js';
+
+const libraryJSON ="picture-library.json";
+let library;  //Global varibale, Loaded async from the current server in window.load event
+
+
+//use the DOMContentLoaded, or window load event to read the library async and render the images
+window.addEventListener('DOMContentLoaded', async () => {
+  
+  library = await lib.pictureLibraryBrowser.fetchJSON(libraryJSON);  //reading library from JSON on local server 
+  //library = lib.pictureLibraryBrowser.createFromTemplate();  //generating a library template instead of reading JSON
+
+  getAlbumTitle(library.albums);
+});
+
+window.addEventListener('click',  () => {
+
+  //just to confirm that the library is accessible as a global variable read async
+  console.log (`library has ${library.albums.length} albums`);
+});
+
+let dropdown = document.getElementById('albumSelect');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Choose Album';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+function getAlbumTitle(data) {  
+  let option;
+  
+  for (const element of data) {
+    option = document.createElement('option');
+    option.text = element.title;
+    option.value = element.id;
+    dropdown.add(option);
+  } 
+};
+
+let titleInput;
+let descriptionInput;
+let fileName;
+let albumInput;
+let option;
+let albumId;
+let albumTitle;
+let selectedAlbumPath;
+
+/*Denna kod är för att få upp bild tillfäligt på browsern*/ 
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+  
+  titleInput = document.getElementById('title').value;
+  descriptionInput = document.getElementById('description').value;
+  fileName = image_input.value.split("\\").pop();
+  albumInput = document.getElementById('albumSelect');
+  option = albumInput.options[albumInput.selectedIndex];
+  albumId = option.value;
+  albumTitle = option.text;
+  selectedAlbumPath = `./library/pictures/${albumTitle}`.replace(/\s+/g, '-').toLowerCase();
+});
+
+const imageUploadForm = document.getElementById('uploadImageForm');
+
+imageUploadForm.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const obj = JSON.parse(library);
+
+  obj[`pictures`].push( { 
+    id: { value: uniqueId(), writable: true, enumerable: true },
+    title: { value: `${titleInput}`, writable: true, enumerable: true },
+    comment: { value: `${descriptionInput}`, writable: true, enumerable: true },
+    imgLoRes: { value: "aaa8913467~small.jpg", writable: true, enumerable: true },
+    imgHiRes: { value: `${fileName}`, writable: true, enumerable: true }
+  })
+
+  lib.pictureLibraryBrowser.postJSON(obj, libraryJSON);
+});
+
+
+
+
+
 /*Rad 3-41 är för att kunna spara bild senare med server denna kod är ej färdig*/ 
 // const http = require("http");
 // const server = http.createServer(function(req,res) {
@@ -43,24 +135,3 @@
 
 
 /*addImage.innerHTML = "Image" + title.value + myFile.value;*/
-
-
-/*Denna kod är för att få upp bild tillfäligt på browsern*/ 
-const image_input = document.querySelector("#image-input");
-
-image_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-  });
-  reader.readAsDataURL(this.files[0]);
-});
-
-
-
-
-
-
-
-
